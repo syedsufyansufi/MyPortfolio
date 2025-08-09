37e0d2480bda93f85a83af8422dfe04bb374e9aa
Syed Sufiyan Ameen <syedsufyan787@gmail.com>
2025-08-10 00:50:10 +0530

    Update Skills.jsx

--- src/sections/Skills/Skills.jsx
+++ src/sections/Skills/Skills.jsx
@@ -12,26 +12,27 @@ function Skills() {
     <section id="skills" className={styles.container}>
       <h1 className="sectionTitle">Skills</h1>
       <div className={styles.skillList}>
-        <SkillList src={checkMarkIcon} skill="HTML" />
-        <SkillList src={checkMarkIcon} skill="CSS" />
-        <SkillList src={checkMarkIcon} skill="JavaScript" />
-        <SkillList src={checkMarkIcon} skill="TypeScript" />
-        <SkillList src={checkMarkIcon} skill="Node" />
-      </div>
-      <hr />
-      <div className={styles.skillList}>
-        <SkillList src={checkMarkIcon} skill="React" />
-        <SkillList src={checkMarkIcon} skill="Angular" />
-        <SkillList src={checkMarkIcon} skill="Vue" />
-        <SkillList src={checkMarkIcon} skill="Tailwind CSS" />
+        <SkillList src={checkMarkIcon} skill="HTML5" />
+        <SkillList src={checkMarkIcon} skill="CSS3" />
+        <SkillList src={checkMarkIcon} skill="JavaScript (ES6+)" />
+        <SkillList src={checkMarkIcon} skill="React.js" />
+        <SkillList src={checkMarkIcon} skill="Node.js" />
       </div>
       <hr />
       <div className={styles.skillList}>
         <SkillList src={checkMarkIcon} skill="Redux" />
-        <SkillList src={checkMarkIcon} skill="Webpack" />
-        <SkillList src={checkMarkIcon} skill="Git" />
-        <SkillList src={checkMarkIcon} skill="Jest" />
-        <SkillList src={checkMarkIcon} skill="Bootstrap" />
+        <SkillList src={checkMarkIcon} skill="Context API" />
+        <SkillList src={checkMarkIcon} skill="Material-UI (MUI)" />
+        <SkillList src={checkMarkIcon} skill="Express.js" />
+        <SkillList src={checkMarkIcon} skill="MongoDB" />
+      </div>
+      <hr />
+      <div className={styles.skillList}>
+        <SkillList src={checkMarkIcon} skill="RESTful APIs" />
+        <SkillList src={checkMarkIcon} skill="Axios" />
+        <SkillList src={checkMarkIcon} skill="Git & GitHub" />
+        <SkillList src={checkMarkIcon} skill="VS Code" />
+        <SkillList src={checkMarkIcon} skill="Postman" />
       </div>
     </section>
   );
